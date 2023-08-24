05dad9ddde098d476f94699dddc2245590f89c9a
Ely Dorado <2018dorado@gmail.com>
2023-08-24 16:11:29 -0500

    databse adjustment and notes

--- notes.js
+++ notes.js
@@ -3,3 +3,28 @@
 //comment made on dev branch
 
 //json-server -p 8088 database.json -w
+
+
+
+/*
+STOP:  Take note of these instructions.  Copy and paste them on the side for later reference.
+ You will likely want to have access to them once the test begins.
+
+
+This is a programming challenge
+If you're expected to write the full code, it'd be mentioned in the code area. All inputs 
+are from STDIN (Standard Input) and output to STDOUT (Standard Output). If you're using Java, 
+use the classname as 'Solution'.  This may be useful - https://www.interviewstreet.com/recruit/challenges/faq/view
+
+To understand more about the environment, time limits, etc. you can read the FAQ here
+
+You will be scored based on all the test cases.  
+Some test cases are hidden but test whether your code applies to all variants of the problem.  
+
+To do as well as possible, please write your solutions to be applicable for all problems, not
+ a specific test case.  
+You do NOT need to solve every question in this test. You only have to solve one question to move 
+on to the next stage in the process. We still encourage you to attempt all of the questions.
+
+Continue
+*/
